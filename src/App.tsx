57b943e1bac178bb836773e5ea3db97c348735c0
Th3rm1t3 <80260010+Th3rm1t0@users.noreply.github.com>
2025-11-29 22:19:41 +0000

    a

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import type React from "react";
+
+export const App: React.FC = () => {
+	return <h1 className="text-9xl">Hello world!</h1>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App.tsx";
+import "./Main.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	createRoot(rootElement).render(
+		<StrictMode>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</StrictMode>,
+	);
+}
